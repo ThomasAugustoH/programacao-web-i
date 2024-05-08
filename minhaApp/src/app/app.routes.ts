14ae9a562a75f3ebb8230eaf3f4b1b4d76292c3b
ThomasAugustoH <thomas.a.haskel@gmail.com>
2024-05-07 21:28:43 -0300

    aula 07.05

--- minhaApp/src/app/app.routes.ts
+++ minhaApp/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AppCadastro2Component } from './view/app-cadastro2/app-cadastro2.component';
 import { Routes } from '@angular/router';
 import { AppHomeComponent } from './view/app-home/app-home.component';
 import { AppCadastroComponent } from './view/app-cadastro/app-cadastro.component';
@@ -11,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'cadastro',
     component: AppCadastroComponent,
+  },
+  {
+    path: 'cadastro2',
+    component: AppCadastro2Component,
   }
 ];
--- minhaApp/src/app/view/app-cadastro/app-cadastro.component.ts
+++ minhaApp/src/app/view/app-cadastro/app-cadastro.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Pessoa } from '../../model/pessoa';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app-cadastro.component.html',
-  styleUrl: './app-cadastro.component.css'
+  styleUrl: './app-cadastro.component.css',
 })
 export class AppCadastroComponent {
+  vPessoa: Pessoa = {
+    nome: 'Thomas',
+    departamento: 'TAH',
+  };
 
+  vetPessoas: Pessoa[] = [
+    {
+      nome: 'Pedro',
+      departamento: 'DM',
+    },
+    {
+      nome: 'Ana',
+      departamento: 'US',
+    },
+  ];
+
+  vNumero: number = 0;
+
+  mostraTamanho(pString: string) {
+    alert("O tamanho da variável é de " + pString.length + " caracteres");
+  }
 }
